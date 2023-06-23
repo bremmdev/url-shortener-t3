875a68e5e1dc0d02c61117987eb4362dc72b05fd
Matt Bremmers <bremmdev@gmail.com>
2023-06-23 23:36:48 +0200

    feat: restrict custom url to certain characters

--- src/schema/url-schema.ts
+++ src/schema/url-schema.ts
@@ -2,5 +2,11 @@ import { z } from "zod";
 
 export const urlInputSchema = z.object({
   url: z.string().max(2048).url({ message: "Invalid url" }),
-  customUrl: z.string().min(4, "Custom URL must be between 4 en 12 characters").max(12, "Custom URL must be between 4 en 12 characters").optional(),
+  customUrl: z
+    .string()
+    .regex(
+      /^[a-zA-Z0-9_-]{4,12}$/,
+      "Length must be 4-12 characters (letters, numbers, - or _)"
+    )
+    .optional(),
 });
